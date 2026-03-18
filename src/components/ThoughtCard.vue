110d7a44588671c8037a4c5b83971dff545b1264
Ibrahim Syed <syedi@oregonstate.edu>
2026-03-17 19:19:59 -0700

    change viewer role to member role

--- src/components/ThoughtCard.vue
+++ src/components/ThoughtCard.vue
@@ -89,7 +89,7 @@ const handleCopy = async () => {
 						<path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
 					</svg>
 				</button>
-				<button v-if="props.isAdmin" @click.stop="emit('edit', thought)" class="p-1.5 text-mono-500 hover:text-rose-bright transition-colors rounded-lg hover:bg-rose/10 cursor-pointer" title="Edit">
+				<button @click.stop="emit('edit', thought)" class="p-1.5 text-mono-500 hover:text-rose-bright transition-colors rounded-lg hover:bg-rose/10 cursor-pointer" title="Edit">
 					<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 						<path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
 						<path d="m15 5 4 4" />
@@ -107,7 +107,7 @@ const handleCopy = async () => {
 						<path d="M12 8v8" /><path d="M8 12h8" />
 					</svg>
 				</button>
-				<button v-if="props.isAdmin" @click.stop="emit('delete', thought.id)" class="p-1.5 text-mono-500 hover:text-red-400 transition-colors rounded-lg hover:bg-red-500/10 cursor-pointer" title="Delete">
+				<button @click.stop="emit('delete', thought.id)" class="p-1.5 text-mono-500 hover:text-red-400 transition-colors rounded-lg hover:bg-red-500/10 cursor-pointer" title="Delete">
 					<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 						<path d="M3 6h18" />
 						<path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
